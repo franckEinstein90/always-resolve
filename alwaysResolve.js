b7d94614881755f7803e0e63e3e72c0e98851822
U-EMERGEDESK\Franck <franckbinard@gmail.com>
2019-12-11 21:26:20 -0500

    simplified things

--- alwaysResolve.js
+++ alwaysResolve.js
@@ -1,28 +1,35 @@
 /**************************************************
  * FranckEinstein90 alwaysResolve 2020
+ * ------------------------------------------------
+ *  single function library alwaysResolve
+ *
+ *  When I use Promises.all for rest API calls, 
+ *  I keep on bumping into use cases
+ *  in which i need the promise to resolve 
+ *  no matter what, because the flow depends on it
+ *
  * ***********************************************/
 "use strict"
 const request = require('request')
-const alwaysResolve = function (apiCall, {
-            test, 
-            good,
-            bad
-        }) {
-            if(test){
-               apiCall = "https://postman-echo.com/get?foo1=bar1&foo2=bar2"
-               good = x => 'ok'
-               bad = x => 'NotOk'
-            }
-            
+const validator = require('validator')
+
+const alwaysResolve = function (apiCall, options = {
+        good,
+        bad
+    }) {
             return new Promise((resolve, reject) => {
-                request(apiCall, function(err, response, body) {
-                    if (err) return resolve(bad)
+               if(! validator.isURL(apiCall)){
+                 return resolve(options.bad("bad url"))
+               } 
+
+               request(apiCall, function(err, response, body) {
+                    if (err) return resolve(options.bad(err))
                     if (response && response.statusCode === 200 && response.statusMessage === "OK") {
-                        resolve(good(body))
+                        resolve(options.good(body))
                     } else {
-                        return resolve(bad)
+                        return resolve(options.bad)
                     }
-                })
+               })
             })
         }
 
